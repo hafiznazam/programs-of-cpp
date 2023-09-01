193acceb50df81e8d84fcfe13a8d4b714e9f778d
hafiznazam <127778592+hafiznazam@users.noreply.github.com>
2023-09-01 10:23:41 +0500

    Add files via upload

---
+++ 2 code.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   //string s;
+   int s , n , f , sum;
+   cin>> s >> n >> f;
+   sum =  s + n + f;
+   cout<< sum <<endl;
+   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
